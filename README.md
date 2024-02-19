92f8ad1af80907b6d23f8b220c9ad6e5800667a7
Stephanie Kwan <skwan@princeton.edu>
2024-02-19 08:12:41 -0600

    initial commit

---
+++ README.md
@@ -0,0 +1,6 @@
+# Pileup weight derivation
+
+```cpp
+n_TrueInt = fs->make<TH1D>("n_TrueInt", "n_TrueInt", 99, 0, 99); # and just fill per event
+```
+
---
+++ make_weight.sh
@@ -0,0 +1,56 @@
+# for name in VBFHToTauTau DYJetsToLL_M-50 DYJetsToLL_M-50-ext1 DYJetsToLL_M-10to50 DY1JetsToLL DY2JetsToLL DY3JetsToLL DY4JetsToLL VVTo2L2Nu WZTo2Q2L WZTo3LNu ZZTo4L ZZTo2Q2L WJetsToLNu W1JetsToLNu W2JetsToLNu W3JetsToLNu W4JetsToLNu GluGluHToTauTau GluGluHToWWTo2L2Nu GluGluZH_HToWWTo2L2Nu GluGluZH_HToWW_ZTo2L HWplusJ_HToWW HWminusJ_HToWW HZJ_HToWW VBFHToWWTo2L2Nu WminusHToTauTau WplusHToTauTau ZHToTauTau ttHToNonbb ttHTobb TTTo2L2Nu TTToHadronic TTToSemiLeptonic ST_tW_antitop; do
+
+for name in SUSYGluGluToHToAA_AToBB_AToTauTau_M-45 SUSYVBFHToAA_AToBB_AToTauTau_M-45; do
+cat > temp.C << EOF
+{
+TFile *g1 = TFile::Open("${name}.root");
+TFile *g2 = TFile::Open("PileupHistogram-goldenJSON-13tev-2018-69200ub-99bins.root");
+TFile *g3 = TFile::Open("PileupHistogram-goldenJSON-13tev-2018-66000ub-99bins.root");
+TFile *g4 = TFile::Open("PileupHistogram-goldenJSON-13tev-2018-72400ub-99bins.root");
+
+TH1D *h1 = (TH1D*)g1->Get("demo/n_TrueInt");
+TH1D *h2 = (TH1D*)g2->Get("pileup");
+TH1D *h3 = (TH1D*)g3->Get("pileup");
+TH1D *h4 = (TH1D*)g4->Get("pileup");
+
+if (h1->Integral()!=0) h1->Scale(1/h1->Integral());
+if (h2->Integral()!=0) h2->Scale(1/h2->Integral());
+if (h3->Integral()!=0) h3->Scale(1/h3->Integral());
+if (h4->Integral()!=0) h4->Scale(1/h4->Integral());
+
+TH1F *weight = new TH1F("puweight_nominal","puweight_nominal",100,0,100);
+TH1F *weight_down = new TH1F("puweight_down","puweight_down",100,0,100);
+TH1F *weight_up = new TH1F("puweight_up","puweight_up",100,0,100);
+
+for(int i = 1; i <= 100; i++){
+  float num = h2->GetBinContent(i);
+  float num_down = h3->GetBinContent(i);
+  float num_up = h4->GetBinContent(i);
+  float den = h1->GetBinContent(i);
+  if(den > 0) { 
+    weight->SetBinContent(i, num/den);
+    weight_down->SetBinContent(i, num_down/den);
+    weight_up->SetBinContent(i, num_up/den); 
+  }
+}
+
+TFile* file = TFile::Open("weightFile_${name}.root","RECREATE");
+h1->Write();
+h2->Write();
+h3->Write();
+h4->Write();
+weight->Write();
+weight_down->Write();
+weight_up->Write();
+file->Close();
+delete weight; delete weight_down; delete weight_up;
+delete file;
+}
+
+EOF
+
+root -l -b -q temp.C
+
+rm -rf temp.C
+
+done
